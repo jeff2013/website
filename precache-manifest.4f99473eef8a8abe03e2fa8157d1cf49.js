09ce565536fc390c431bf718bfa6ed38126588b7
Jeff Chang <jeffreychang2010@gmail.com>
2019-12-28 14:29:02 -0800

    Updates

---
+++ precache-manifest.4f99473eef8a8abe03e2fa8157d1cf49.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4afb511689798c70b6927cbe5e5e0e",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "34530159e5af9f0b778d",
+    "url": "/myapp/static/css/main.08aa19b0.chunk.css"
+  },
+  {
+    "revision": "25533bdea7352fa06830",
+    "url": "/myapp/static/js/2.9bb61ecf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.9bb61ecf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34530159e5af9f0b778d",
+    "url": "/myapp/static/js/main.7bc5f715.chunk.js"
+  },
+  {
+    "revision": "abb0a8ae49457810f009",
+    "url": "/myapp/static/js/runtime-main.b64a088d.js"
+  },
+  {
+    "revision": "20fdd0742017cab4a467cc94ae75dd9d",
+    "url": "/myapp/static/media/about-me.20fdd074.JPG"
+  },
+  {
+    "revision": "83591a90a1d2c6cbbdd635a52d3c127c",
+    "url": "/myapp/static/media/github.83591a90.svg"
+  },
+  {
+    "revision": "fed3c76e4840f0a57736f52c05241b49",
+    "url": "/myapp/static/media/instagram.fed3c76e.svg"
+  },
+  {
+    "revision": "aae06753c363a70b187fac67bd1364dc",
+    "url": "/myapp/static/media/linkedin.aae06753.svg"
+  },
+  {
+    "revision": "2060a0c703de3b2124e950c3ab6c1951",
+    "url": "/myapp/static/media/photo-1.2060a0c7.jpg"
+  },
+  {
+    "revision": "a0dba912fd43481422fd2ea77612ca9c",
+    "url": "/myapp/static/media/photo-2.a0dba912.jpg"
+  },
+  {
+    "revision": "7f2e0fc93bf0a865a372c0f8b5d11f95",
+    "url": "/myapp/static/media/photo-3.7f2e0fc9.jpg"
+  }
+]);
